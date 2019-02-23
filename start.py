2ff7c1eddd24118fcf3121da3a3b5d418a354c81
munhyunsu <munhyunsu@gmail.com>
2019-02-23 14:32:46 +0900

    Bug fix for calculating avg

--- start.py
+++ start.py
@@ -6,6 +6,9 @@ from operator import itemgetter
 
 from scrap_data import get_data
 
+ARGS = None
+
+
 def main():
     with open('asn.pickle', 'rb') as f:
         asn_data = pickle.load(f)
@@ -14,13 +17,14 @@ def main():
     ctr = input('Input the country code: ')
     ctr = ctr.strip()
     
-    print('ASNs: {0}'.format(asn_data[ctr]))
+    print('The number of ASNs: {0}'.format(len(asn_data[ctr])))
 
     asn_hege = dict()
     asn_name = dict()
+    active_asn = 0
 
-    for asn in asn_data[ctr]:
-        for result in get_data(asn)['results']:
+    for origin_asn in asn_data[ctr]:
+        for result in get_data(origin_asn)['results']:
             asn = int(result['asn'])
             hege = float(result['hege'])
             name = result['asn_name']
@@ -28,16 +32,30 @@ def main():
                 asn_hege[asn] = list()
             asn_hege[asn].append(hege)
             asn_name[asn] = name
+            if asn == int(origin_asn):
+                active_asn = active_asn + 1
 
     asn_avg = list()
     for key in asn_hege.keys():
-        asn_avg.append((key, statistics.mean(asn_hege[key])))
+        asn_avg.append((key, sum(asn_hege[key])/active_asn))
     asn_avg.sort(key=itemgetter(0), reverse=False)
     asn_avg.sort(key=itemgetter(1), reverse=True)
 
     for key, value in asn_avg:
-        print('ASN: {0:7d} - {1:1.4f}'.format(key, value))
+        if ARGS.verbosity is not None:
+            name = asn_name[key]
+            print('ASN: {0:7d} - {1:1.4f} ({2})'.format(key, value, name))
+        else:
+            print('ASN: {0:7d} - {1:1.4f}'.format(key, value))
 
 
 if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--verbosity', action='count',
+                        help='increase output verbosity')
+
+    ARGS = parser.parse_args()
+
     main()
