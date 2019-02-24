6c2a3b8c2637cb23f2391419f40e62f71f17c12c
munhyunsu <munhyunsu@gmail.com>
2019-02-24 09:07:00 +0900

    Zeroday script

---
+++ 0-zero-day.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Download ASN files"
+wget https://ftp.ripe.net/ripe/asnames/asn.txt -O asn.txt
+
+echo "Convert file encoding to UTF-8"
+iconv -f LATIN1 -t UTF-8 asn.txt -o asn_utf-8.txt
+mv asn_utf-8.txt asn.txt
+
+echo "Create python3 venv"
+python3 -m venv ./venv
+
+echo "Install(pip3) some package"
+source ./venv/bin/activate
+pip3 install --upgrade pip setuptools
+pip3 install -r requirements.txt
+
+echo "Create pickle"
+python3 1-create_asn_country_pickle.py
+
+echo "Done! you can execute start.py"
+echo "Command: python3 start.py"
+
